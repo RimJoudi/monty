c316d655f62901ffe7bd008cf6436147c6a12b6f
maroua199525 <2646@holbertonschool.com>
2021-05-13 16:04:25 +0100

    add stderr

--- monty.c
+++ monty.c
@@ -20,13 +20,13 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read != -1)
