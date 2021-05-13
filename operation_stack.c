d020c0f2b4d1d39e9881282deccfa44e3527e2eb
RimJoudi <joudirim93@gmail.com>
2021-05-13 15:58:01 +0100

    fix the stderr condition

--- operation_stack.c
+++ operation_stack.c
@@ -28,7 +28,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		printf("L%u: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
