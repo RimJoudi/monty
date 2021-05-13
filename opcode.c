9df75ae4d814650b066cc21dbfa50d0cfb103a33
maroua199525 <2646@holbertonschool.com>
2021-05-13 14:58:42 +0100

    get opcode

---
+++ opcode.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ *get_opcode - get the opcode of a Monty file 
+ *
+ *Return: pointer to a string containing the opcode
+ */
+char *get_opcode(void)
+{
+	int i = 0, j = 0;
+	char *buffer;
+    size_t len = 20;
+
+	buffer = malloc(sizeof(char) * len);
+	if (buffer == NULL)
+		return (NULL);
+
+	while (line[i] == ' ')
+		i++;
+	while (line[i] != ' ' && line[i] != '\0')
+	{
+		buffer[j] = line[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+	return (buffer);
+}
