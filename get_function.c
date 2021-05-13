953282cc715354c74205f9d14a2bf0de865ed008
maroua199525 <2646@holbertonschool.com>
2021-05-13 15:07:46 +0100

    get function

---
+++ get_function.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+/**
+ *get_func - compares opcodes and runs associated functions
+ *@stack: pointer to the top of the stack
+ *@line_number: line number of command
+ *@opcode: the opcode 
+ *
+ *Return: void
+ */
+void get_func(stack_t **stack, unsigned int line_number, char *opcode)
+{
+	int i = 0;
+
+	instruction_t func[] = {
+		{"push", push}, 
+        {"pall", pall}, 
+        {"pint", pint},
+		{"pop", pop}, 
+        {"swap", swap}, 
+        {"add", add},
+		{"nop", nop}, 
+        {"sub", sub}, 
+        {"div", division},
+		{"mul", mul}, 
+        {"mod", mod},
+		{NULL, NULL}
+	};
+
+	while (func[i].opcode != NULL)
+	{
+		if (strcmp(opcode, func[i].opcode) == 0)
+		{
+			func[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+
+	printf("L%u: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
