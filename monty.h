20871a39e73e8d5ee4c6168ee84bc8ff9fce2a79
RimJoudi <joudirim93@gmail.com>
2021-05-13 15:54:01 +0100

    add ;

--- monty.h
+++ monty.h
@@ -45,7 +45,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number)
+void pchar(stack_t **stack, unsigned int line_number);
 /***********Arithmetique_Functio*******************/
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
